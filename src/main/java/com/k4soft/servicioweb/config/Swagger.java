534583edd74e7ab4ed698340555e606982f78ad3
diegogarcia <consultor@dgpragmatic.com>
2020-03-12 19:29:01 -0500

    se crea servicios post para persona

---
+++ src/main/java/com/k4soft/servicioweb/config/Swagger.java
@@ -0,0 +1,10 @@
+package com.k4soft.servicioweb.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger {
+
+}
